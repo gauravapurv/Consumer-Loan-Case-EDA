f30add541a3f202232f7bedfae7367419996d1e6
gauravapurv <write2apurv@gmail.com>
2020-03-15 20:59:41 -0400

    Changes

--- loan_case_study.R
+++ loan_case_study.R
@@ -12,3 +12,17 @@ library(dplyr)
 #Importing the loan.csv file in the dataframe for analysis.
 loan_master <- read.csv("loan.csv", stringsAsFactors = F)
 
+#Structure of loan_master dataframe.
+str(loan_master)
+
+# Summary of loan_master frame
+sapply(loan_master, summary)
+# Total columns in the data frame
+colnames(loan_master)
+
+#Checking for missing values in the dataframe.
+sum(is.na(loan_master))
+
+
+
+
